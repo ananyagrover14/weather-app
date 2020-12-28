ac380331e47192c1b1f446b2903e0b35923dbab7
Ananya Grover <ananya.grover2017@gmail.com>
2020-12-28 13:24:24 +0530

    Add error.html

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def temperature():
 	cityname = (request.form['name']).capitalize()
 	r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+cityname+'&appid=545b6bc00ae6265f871ac2212005c0c0')
-	json_object = r.json() 
+	json_object = r.json()
+	if json_object['cod'] == '404':
+	  return render_template("error.html", message = "City not found") 
 	temp_k = float(json_object['main']['temp'])
 	temp_c = round((temp_k - 273.15) , 2)
 	mintemp_k = float(json_object['main']['temp_min'])
